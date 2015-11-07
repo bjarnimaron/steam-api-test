90d2274ea27c9a2cc719d477b4ec715cff95c34f
Bjarni Maron Sigurðsson <bjarnimaron@gmail.com>
2015-11-07 14:34:50 +0000

    DemoInfo parser included

--- steam-api-test/Program.cs
+++ steam-api-test/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Reflection;
+using System.IO;
+using DemoInfo;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace steam_api_test
 {

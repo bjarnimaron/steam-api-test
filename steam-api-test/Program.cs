d43961d7a0654cf75e7b52c6c6b852abe0146a72
Bjarni Maron Sigurðsson <bjarnimaron@gmail.com>
2015-11-08 16:21:35 +0000

    Bætti við leið til að sækja nafn á leikmanni sem er checkaður

--- steam-api-test/Program.cs
+++ steam-api-test/Program.cs
@@ -25,12 +25,15 @@ namespace steam_api_test
         {
             string playerID = "76561197963115149";
 
-            GetVacBan(sendRequest(playerID));
+            parseForName(nameRequest(playerID));
+            parseForBan(banRequest(playerID));
+
+            
             Console.ReadKey();
         }
 
         //Tekur við XML strengnum frá sendRequest og les úr honum gildin sem við viljum fá
-        static void GetVacBan(string xmlInput)
+        static void parseForBan(string xmlInput)
         {
             StringBuilder output = new StringBuilder();
 
@@ -39,28 +42,57 @@ namespace steam_api_test
 
             using (XmlReader reader = XmlReader.Create(new StringReader(xmlInput), settings))
             {
-                reader.ReadToFollowing("SteamId");
-                output.AppendLine("SteamId: " + reader.ReadElementContentAsString());
+                    reader.ReadToFollowing("SteamId");
+                    output.AppendLine("SteamId: " + reader.ReadElementContentAsString());
 
-                reader.ReadToFollowing("VACBanned");
-                output.AppendLine("VAC Banned?:  " + reader.ReadElementContentAsString());
+                    reader.ReadToFollowing("VACBanned");
+                    output.AppendLine("VAC Banned:  " + reader.ReadElementContentAsString());
             }
 
             Console.WriteLine(output);
-
         }
+
+        static void parseForName(string xmlInput)
+        {
+            StringBuilder output = new StringBuilder();
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlInput), settings))
+            {
+                    reader.ReadToFollowing("personaname");
+                    output.Append("Player name: " + reader.ReadElementContentAsString());
+            }
+
+            Console.WriteLine(output);
+        }
+
 
         //Sendir HTTP beiðni á Steam Community og sækir bans á einhverjum ákveðnum leikmanni, skilar svo sem XML streng
-        static string sendRequest(string steamId)
+        static string banRequest(string steamId)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=" + getAPIKey() + "&steamids=" + steamId + "&format=xml");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=" + getAPIKey() + "&steamids=" + steamId + "&format=xml");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            StreamReader stream = new StreamReader(response.GetResponseStream());
+                StreamReader stream = new StreamReader(response.GetResponseStream());
 
-            string xmlResult = stream.ReadToEnd();
+                string xmlResult = stream.ReadToEnd();
 
-            return xmlResult;
+                return xmlResult;
+        } 
+        
+        static string nameRequest(string steamId)
+        {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + getAPIKey() + "&steamids=" + steamId + "&format=xml");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader stream = new StreamReader(response.GetResponseStream());
+
+                string xmlResult = stream.ReadToEnd();
+
+                return xmlResult;
         }
+
     }
 }

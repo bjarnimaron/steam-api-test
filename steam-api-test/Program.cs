f80bf28d925a19cfa31cc13114672e1b43041cd4
Bjarni Maron Sigurðsson <bjarnimaron@gmail.com>
2015-11-08 15:48:55 +0000

    Bætti við method til að sækja VAC ban m.v. steamID

--- steam-api-test/Program.cs
+++ steam-api-test/Program.cs
@@ -4,22 +4,61 @@ using System.IO;
 using DemoInfo;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using System.Text;
+using System.Xml;
 
 
 namespace steam_api_test
 {
     class Program
     {
+        //Gets the Steam Web API key from file
         static String getAPIKey()
         {
-            StreamReader parse = File.OpenText("apikey.xml");
+            StreamReader parse = File.OpenText("apikey");
             return parse.ReadLine();
         }
 
         static void Main(string[] args)
         {
-            System.Console.WriteLine(getAPIKey());
-            System.Console.ReadKey();
+            string playerID = "76561197963115149";
+
+            GetVacBan(sendRequest(playerID));
+            Console.ReadKey();
+        }
+
+        static void GetVacBan(string xmlInput)
+        {
+            StringBuilder output = new StringBuilder();
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlInput), settings))
+            {
+                reader.ReadToFollowing("SteamId");
+                output.AppendLine("SteamId: " + reader.ReadElementContentAsString());
+
+                reader.ReadToFollowing("VACBanned");
+                output.AppendLine("VAC Banned?:  " + reader.ReadElementContentAsString());
+            }
+
+            Console.WriteLine(output);
+
+        }
+
+        static string sendRequest(string steamId)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=" + getAPIKey() + "&steamids=" + steamId + "&format=xml");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+
+            string xmlResult = stream.ReadToEnd();
+
+            return xmlResult;
         }
     }
 }
